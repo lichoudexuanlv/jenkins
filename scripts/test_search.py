06ad5cc708c224637e742fdcc19e7536c3ef5690
lichoudexuanlv <1066799163@qq.com>
2019-03-08 11:14:17 +0800

    第一次添加

---
+++ scripts/test_search.py
@@ -0,0 +1,17 @@
+
+
+class TestSearch:
+
+    def test01(self):
+        assert 1
+
+
+    def test02(self):
+        assert 0
+
+
+    def test03(self):
+        assert 1
+
+    def test04(self):
+        assert 1
